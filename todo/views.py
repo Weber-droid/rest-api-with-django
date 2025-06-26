3165c8760bb2dad1058da6fd41291ef79fa21a99
Weber-droid <emmanueltotimeh2002@gmail.com>
2025-06-26 10:43:28 +0000

    fixxx

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from .models import Task
+from .serializer import TaskSerializer
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
